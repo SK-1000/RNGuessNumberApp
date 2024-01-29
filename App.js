7e9805536a12e08378f215e3502ace95847b9025
sk1001 <sheilafleming81@gmail.com>
2024-01-29 19:30:50 +0000

    Guess the number higher lower functality

--- App.js
+++ App.js
@@ -3,22 +3,36 @@ import { StyleSheet, ImageBackground, SafeAreaView} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 import Colors from './constants/colors';
 
 //we will want to swap the Start Game Screen with the Game screen as soon as we have a number
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(); // registering state here. The number picked by the user. We initially set it as null or undefined
+  const [gameIsOver, setGameIsOver] = useState(true);
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+
+
+  function gameOverHandler() {
+    setGameIsOver(true);
   }
 
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
 
   if (userNumber) {
-    screen = <GameScreen/>
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
   }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />;
+  }
+
+
 
   return (
     <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
 
-function NumberContainer () {
-return <View>
-<Text></Text>
-</View>
-
+function NumberContainer({ children }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.numberText}>{children}</Text>
+    </View>
+  ); //The view here allows me to add borderRadious in styling which rounds the coners. This only works in view and not text.
 }
 export default NumberContainer;
 
-
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    padding: 24,
+    margin: 24,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  numberText: {
+    color: Colors.accent500,
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+});
